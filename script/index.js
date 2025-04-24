4a4880a09dd1cc3c985e8c28c26769c59ffa52b2
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-04-24 19:37:38 +0300

    pushing

---
+++ script/index.js
@@ -0,0 +1,23 @@
+let bod = document.body;
+
+let reg_button = document.getElementById("reg");
+let login_button = document.getElementById("login");
+
+let close_reg = document.getElementById("reg_close");
+let close_log = document.getElementById("log_close")
+
+reg_button.addEventListener("click", function (){
+    bod.classList.add ("overf-hid");
+});
+
+login_button.addEventListener("click", function (){
+    bod.classList.add ("overf-hid");
+});
+
+close_reg.addEventListener("click", function (){
+    bod.classList.remove ("overf-hid")
+});
+
+close_log.addEventListener("click", function (){
+    bod.classList.remove ("overf-hid")
+});
